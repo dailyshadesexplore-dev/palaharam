b27e6c218ba9bfbdf7e33df8e3e297a7b7ff1897
DEV-HUSTLING <anushkadevg@gmail.com>
2026-07-12 17:01:14 -0400

    fixing build issues

--- palaharam_BE/main.py
+++ palaharam_BE/main.py
@@ -25,4 +25,4 @@ app.add_middleware(
 if __name__=="__main__":
     import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="127.0.0.1", port=int(os.environ.get("PORT", 8000)))
